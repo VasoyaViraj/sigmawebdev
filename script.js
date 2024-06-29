56fd9e443bc6293eebd460db0c8fa079c13513bd
VasoyaViraj <108251761+VasoyaViraj@users.noreply.github.com>
2024-06-29 17:30:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll('.button');
+let main = document.querySelector("main");
+
+buttons.forEach( function(button){
+    // console.log(button);
+    button.addEventListener("click", function(e){
+        // console.log(e.target);
+        // console.log(e.target.id);
+        if(e.target.id == "red"){
+            main.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == "yellowgreen"){
+            main.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == "yellow"){
+            main.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == "pink"){
+            main.style.backgroundColor = e.target.id
+        }
+    })
+} )
