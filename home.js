e0065c6399395c7c4bc1e4af467bb31526afee2a
VasoyaViraj <108251761+VasoyaViraj@users.noreply.github.com>
2024-07-01 00:02:19 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,18 @@
+let div_projects = document.querySelectorAll('.box');
+
+div_projects.forEach(function(box){
+    box.addEventListener("click",function(e){
+        if(e.target.id == "project_1"){
+            window.location.replace("./project_1.html");
+        }
+        if(e.target.id == "project_2"){
+            window.location.replace("./project_2.html");
+        }
+    })
+})
+
+
+
+
+
+
---
+++ nav.js
@@ -0,0 +1,5 @@
+let home = document.querySelector("#home");
+
+home.addEventListener("click",function(){
+    window.location.replace("./index.html");
+})
