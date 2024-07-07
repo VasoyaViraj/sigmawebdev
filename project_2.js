bc30b70e7a9ea5211145d5d3091bc5e0b00b58f9
VasoyaViraj <108251761+VasoyaViraj@users.noreply.github.com>
2024-07-07 21:06:42 +0530

    Update project_2.js

--- project_2.js
+++ project_2.js
@@ -1,13 +1,17 @@
+let billdev = document.querySelector(".bill")
+let app = document.querySelector(".app")
+let audio = new Audio("/sound.wav")
+
 function oncart(){
     var check = false;
-    var cookievalue ="";
+    var cook ="";
 
     if(document.getElementById("c1").checked){
         check = true;
         var quantity = document.getElementById("q1").value;
         var price = 200;
         var total = (price * quantity);
-        cookievalue += ":School Bag," + price +","+quantity+"," +total;
+        cook += ":School Bag," + price +","+quantity+"," +total;
     }
 
     if(document.getElementById("c2").checked){
@@ -15,7 +19,7 @@ function oncart(){
         var quantity = document.getElementById("q2").value;
         var price = 150;
         var total = (price * quantity);
-        cookievalue += ":Water Bottle," + price +","+quantity+"," +total;
+        cook += ":Water Bottle," + price +","+quantity+"," +total;
     }
 
     if(document.getElementById("c3").checked){
@@ -23,7 +27,7 @@ function oncart(){
         var quantity = document.getElementById("q3").value;
         var price = 50;
         var total = (price * quantity);
-        cookievalue += ":Notebook," + price +","+quantity+"," +total;
+        cook += ":Notebook," + price +","+quantity+"," +total;
     }
 
     if(document.getElementById("c4").checked){
@@ -31,15 +35,43 @@ function oncart(){
         var quantity = document.getElementById("q4").value;
         var price = 20;
         var total = (price * quantity);
-        cookievalue += ":Lunch Box," + price +","+quantity+"," +total;
+        cook += ":Lunch Box," + price +","+quantity+"," +total;
     }
 
     if(!check)
     {
         alert("No Item Selected");
     }
+
     else{
-        document.cookie = cookievalue;
-        window.location="project_2_bill.html"
+        app.querySelector(".main").classList.remove("active");
+        app.querySelector(".bill").classList.add("active");
+
+        var bill = document.querySelector('.bill');
+        // var cook = ":School Bag,200,2,400:Water Bottle,150,2,300:Notebook,50,2,100:Lunch Box,20,10,200";
+        var cookiearr = new Array();
+        cookiearr = cook.split(":");
+        cookiearr.shift();
+
+        var tb1 =document.createElement("table");
+        var tb1body = document.createElement("tbody");
+        var thead = document.createElement("thead")
+        thead.innerHTML = "<tr><th>Description</th><th>Price/item</th><th>Quantity</th><th>Total</th></tr>"
+        tb1.appendChild(thead)
+
+        for (let i = 0; i < cookiearr.length; i++) {
+        var cookiedata = cookiearr[i].split(",");
+        var row = document.createElement("tr")
+            for (let j = 0; j < 4; j++) {
+                var cell = document.createElement("td")
+                var cellText = document.createTextNode(`${cookiedata[j]}`)
+                cell.appendChild(cellText)
+                row.appendChild(cell)
+            }
+            tb1.appendChild(row)
+        }
+
+        tb1.appendChild(tb1body)
+        bill.appendChild(tb1)
     }
 }
