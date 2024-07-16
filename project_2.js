1c674a3c327fcea5d40ad31ba64f791eaba83939
VasoyaViraj <108251761+VasoyaViraj@users.noreply.github.com>
2024-07-16 15:02:41 +0530

    Update project_2.js

--- project_2.js
+++ project_2.js
@@ -1,5 +1,5 @@
-
-let billdev = document.querySelector(".bill")
+var bill = document.getElementById("bill")
+var main = document.getElementById("main")
 let app = document.querySelector(".app")
 let audio = new Audio("/sound.wav")
 
@@ -45,44 +45,42 @@ function oncart(){
     }
 
     else{
-        app.querySelector(".main").classList.remove("active");
-        app.querySelector(".bill").classList.add("active");
+        main.style.display="none";
+        bill.style.display="flex";
 
-        var bill = document.querySelector('.bill');
-//var cook = ":School Bag,200,2,400:Water Bottle,150,2,300:Notebook,50,2,100:Lunch Box,20,10,200";
-var cookiearr = new Array();
-cookiearr = cook.split(":");
-cookiearr.shift();
+        var cookiearr = new Array();
+        cookiearr = cook.split(":");
+        cookiearr.shift();
 
-var tb1 =document.createElement("table");
-var tb1body = document.createElement("tbody");
-var thead = document.createElement("thead")
-thead.innerHTML = "<tr><th>Description</th><th>Price</th><th>Quantity</th><th>Total</th></tr>"
-tb1.appendChild(thead)
+        var tb1 =document.createElement("table");
+        var tb1body = document.createElement("tbody");
+        var thead = document.createElement("thead")
+        thead.innerHTML = "<tr><th>Description</th><th>Price</th><th>Quantity</th><th>Total</th></tr>"
+        tb1.appendChild(thead)
 
-var total2=0;
-var total3=0;
+        var total2=0;
+        var total3=0;
 
-for (let i = 0; i < cookiearr.length; i++) {
-    var cookiedata = cookiearr[i].split(",");
-    var row = document.createElement("tr")
-    for (let j = 0; j < 4; j++) {
-        var cell = document.createElement("td")
-        var cellText = document.createTextNode(`${cookiedata[j]}`)
-        cell.appendChild(cellText)
-        row.appendChild(cell)
-        
-    }
-    total2 += parseFloat(cookiedata[2]);
-    total3 += parseFloat(cookiedata[3]);
-    tb1.appendChild(row)
-}
+        for (let i = 0; i < cookiearr.length; i++) {
+            var cookiedata = cookiearr[i].split(",");
+            var row = document.createElement("tr")
+            for (let j = 0; j < 4; j++) {
+                var cell = document.createElement("td")
+                var cellText = document.createTextNode(`${cookiedata[j]}`)
+                cell.appendChild(cellText)
+                row.appendChild(cell)
+                
+            }
+            total2 += parseFloat(cookiedata[2]);
+            total3 += parseFloat(cookiedata[3]);
+            tb1.appendChild(row)
+        }
 
-var ttotal = document.createElement("tr")
-ttotal.innerHTML=`<td>Total</td><td></td><td>${total2}</td><td>${total3}</td>`
-tb1.appendChild(ttotal);
+        var ttotal = document.createElement("tr")
+        ttotal.innerHTML=`<td colspan="2">Total</td><td>${total2}</td><td>${total3}</td>`
+        tb1.appendChild(ttotal);
 
-tb1.appendChild(tb1body)
-bill.appendChild(tb1)
+        tb1.appendChild(tb1body)
+        bill.appendChild(tb1)
     }
 }
